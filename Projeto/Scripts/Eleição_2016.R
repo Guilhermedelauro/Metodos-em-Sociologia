c3952e06c73585a3607aa6ecc022c3573a4e3086
Guilhermedelauro <guidelauro@gmail.com>
2026-05-06 13:42:38 -0300

    organiza os scripts

--- Projeto/Scripts/Eleição_2016.R
+++ Projeto/Scripts/Eleição_2016.R
@@ -1,16 +1,24 @@
 
+# 1. Bibliotecas e pacotes utilizados
 
 Install.packages("tidyverse") #Instalar as bibliotecas
 install.packages("dplyr")
+install.packages("janitor")
+install.packages("broom")
+install.packages("car")
 
 library(tidyverse)  #Carregar as bibliotecas
 library(dplyr)
+library(janitor)
+library(broom)
+library(car)
 
-
+#----------
+# 2.Criação dos bancos de dados
 
 #Importa o banco de dados de 2016
 #Retira os candidatos indígenas e amarelos
-#Cria uma nova váriavel se diz se o candiato é branco ou não-branco
+#Cria uma nova váriavel que diz se o candiato é branco ou não-branco
 candidatos_2016<- read.csv2("C:\\Users\\guide\\OneDrive\\R pasta\\Metodos-em-Sociologia\\Banco_2016.csv", fileEncoding = "latin1")
 
 candidatos_filtrado_2016 <- candidatos_2016 %>%
@@ -20,27 +28,23 @@ candidatos_filtrado_2016 <- candidatos_2016 %>%
 table(candidatos_2016$DS_COR_RACA) #vê a quiantidade de candidatos por raça  
 
 
-
 #A partir da tabela de candidatos filtra para os eleitos e coloca em ordem decrescente pelo número de votos
+#Cria uma nova variável que diz se o candidato é competitivo ou não 
+#Os competitivos tem pelo menos 2493 votos (20% do número de votos do último eleito)
 eleitos2016 <- candidatos_2016 %>%
   filter(DS_SIT_TOT_TURNO %in% c("ELEITO POR MÉDIA", "ELEITO POR QP")) %>%
   arrange(desc(TOTAL_VOTOS))
 
-#o último eleito recebeu 12464 votos
-
-
-
-#Cria uma nova variável que diz se o candidato é competitivo ou não 
-#Os competitivos tem pelo menos 2493 votos (20% do número de votos do último eleito)
 candidatos_competitivos2016 <- candidatos_filtrado_2016 %>%
   mutate(COMPETITIVO = ifelse(TOTAL_VOTOS >= 2493, "Sim", "Não"))
 
 
 
-#Cria uma nova variável que divide os competitivos em decis
+#Cria uma nova base onde os competitivos estão divididos em decil
 candidatos_decis2016 <- candidatos_competitivos2016 %>%
   filter(COMPETITIVO == "Sim") %>%
-  mutate(DECIL = ntile(TOTAL_VOTOS, 10))
+  arrange(RECEITA_TOTAL) %>%
+  mutate(DECIL = ntile(RECEITA_TOTAL, 10))
 
 candidatos_decis2016 %>%   #mostra o máximo e mínimo de cada decil
   group_by(DECIL) %>%
@@ -48,3 +52,24 @@ candidatos_decis2016 %>%   #mostra o máximo e mínimo de cada decil
     minimo = min(TOTAL_VOTOS, na.rm = TRUE),
     maximo = max(TOTAL_VOTOS, na.rm = TRUE),
     .groups = "drop")
+
+#------------
+# 3.Tratamento dos bancos de dados
+
+
+#----------
+# 4.Operações com os bancos de dados
+
+#Faz a proporção de candidatos brancos e não-brancos
+prop.table(table(candidatos_competitivos2016$RACA_AGRUPADA)) * 100
+
+#Faz a proporção de candidatos brancos e não-brancos para os competitivos
+candidatos_competitivos2016 %>%
+  filter(COMPETITIVO == "Sim") %>%
+  with(prop.table(table(RACA_AGRUPADA)) * 100)
+  
+#Faz a proporção de candidatos brancos e não-brancos para os eleitos 
+candidatos_competitivos2016 %>%
+  filter(DS_SIT_TOT_TURNO %in% c("ELEITO POR QP", "ELEITO POR MÉDIA")) %>%
+  with(prop.table(table(RACA_AGRUPADA)) * 100)
+
--- Projeto/Scripts/Eleição_2020.R
+++ Projeto/Scripts/Eleição_2020.R
@@ -1,12 +1,20 @@
 
+# 1. Bibliotecas e pacotes utilizados
 
 Install.packages("tidyverse") #Instalar as bibliotecas
 install.packages("dplyr")
+install.packages("janitor")
+install.packages("broom")
+install.packages("car")
 
 library(tidyverse)  #Carregar as bibliotecas
 library(dplyr)
+library(janitor)
+library(broom)
+library(car)
 
-
+#----------
+# 2.Criação dos bancos de dados
 
 #Importa o banco de dados de 2020
 #Retira os candidatos indígenas e amarelos
@@ -20,27 +28,22 @@ candidatos_filtrado_2020 <- candidatos_2020 %>%
 table(candidatos_2020$DS_COR_RACA) #vê a quiantidade de candidatos por raça  
 
 
-
 #A partir da tabela de candidatos filtra para os eleitos e coloca em ordem decrescente pelo número de votos
+#Cria uma nova variável que diz se o candidato é competitivo ou não 
+#Os competitivos tem pelo menos 2735 votos (20% do número de votos do último eleito)
 eleitos2020 <- candidatos_2020 %>%
   filter(DS_SIT_TOT_TURNO %in% c("ELEITO POR MÉDIA", "ELEITO POR QP")) %>%
   arrange(desc(TOTAL_VOTOS))
 
-#o último eleito recebeu 13673 votos
-
-
-
-#Cria uma nova variável que diz se o candidato é competitivo ou não 
-#Os competitivos tem pelo menos 2735 votos (20% do número de votos do último eleito)
 candidatos_competitivos2020 <- candidatos_filtrado_2020 %>%
   mutate(COMPETITIVO = ifelse(TOTAL_VOTOS >= 2735, "Sim", "Não"))
 
 
-
-#Cria uma nova variável que divide os competitivos em decis
+#Cria uma nova base onde os candidatos competitivos estão divididos em decis de receita
 candidatos_decis2020 <- candidatos_competitivos2020 %>%
   filter(COMPETITIVO == "Sim") %>%
-  mutate(DECIL = ntile(TOTAL_VOTOS, 10))
+  arrange(RECEITA_TOTAL) %>%
+  mutate(DECIL = ntile(RECEITA_TOTAL, 10))
 
 candidatos_decis2020 %>%   #mostra o máximo e mínimo de cada decil
   group_by(DECIL) %>%
@@ -49,5 +52,24 @@ candidatos_decis2020 %>%   #mostra o máximo e mínimo de cada decil
     maximo = max(TOTAL_VOTOS, na.rm = TRUE),
     .groups = "drop")
 
+#------------
+# 3.Tratamento dos bancos de dados
 
+
+
+#----------
+# 4.Operações com os bancos de dados
+
+#Faz a proporção de candidatos brancos e não-brancos
+prop.table(table(candidatos_competitivos2020$RACA_AGRUPADA)) * 100
+
+#Faz a proporção de candidatos brancos e não-brancos para os competitivos
+candidatos_competitivos2020 %>%
+  filter(COMPETITIVO == "Sim") %>%
+  with(prop.table(table(RACA_AGRUPADA)) * 100)
+
+#Faz a proporção de candidatos brancos e não-brancos para os eleitos 
+candidatos_competitivos2020 %>%
+  filter(DS_SIT_TOT_TURNO %in% c("ELEITO POR QP", "ELEITO POR MÉDIA")) %>%
+  with(prop.table(table(RACA_AGRUPADA)) * 100)
 
